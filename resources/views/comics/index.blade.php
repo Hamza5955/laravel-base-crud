7d265fe41ad77fba3a35e1f291f4cf0cb4513c88
Hamza <Hamza5955>
2021-11-04 09:07:14 +0100

    section

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,38 @@
 
 @section('content')
     <div class="container">
-        <h1>sono indice della pasta</h1>
-        @dump($comic)
+        <div class="row">
+            <div class="col-12">
+                <h1 class="mt-4">Indice dei fumetti</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Descrizone</th>
+                            <th scope="col">Prezzo</th>
+                            <th scope="col">Serie</th>
+                            <th scope="col">Data di vendita</th>
+                            <th scope="col">Genere</th>
+                     
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comic as $comic)
+                            <tr>
+                                <th scope="row">{{ $comic['id'] }}</th>
+                                <td>{{ $comic['title'] }}</td>
+                                <td>{!! $comic['description'] !!}</td>
+                                <td>{{ $comic['price'] }}€</td>
+                                <td>{{ $comic['series'] }}</td>
+                                <td>{{ $comic['sale_date'] }}</td>
+                                <td>{{ $comic['type'] }}</td>
+                                
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
